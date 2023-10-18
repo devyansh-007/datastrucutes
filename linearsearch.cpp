6e7f27625b39d407c622108f19090fbfd75ac1b7
devyansh-007 <129656658+devyansh-007@users.noreply.github.com>
2023-10-18 11:17:47 +0530

    Update linearsearch.cpp

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -1,6 +1,5 @@
 //linear search
-#include <bits/stdc++.h>
-using namespace std;
+
 int linearSearch(int arr[], int size, int target) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == target) {
