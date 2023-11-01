fec288f8ac0026dd227363b7e878096d470031b2
devyansh-007 <129656658+devyansh-007@users.noreply.github.com>
2023-11-01 07:02:44 +0530

    Create 46 height of tree.cpp

---
+++ 46 height of tree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildingtree(node*root){
+    int data;
+    cout << "enter the data" << endl;
+    cin >> data;
+    root = new node(data);
+    if(data == -1){
+        return NULL;
+    }
+    cout << "enter the left value of " << data << endl;
+    root -> left = buildingtree(root -> left);
+    cout << "enter the right value of " << data << endl;
+    root -> right = buildingtree(root -> right);
+    return root;
+}
+int height(node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int left = height(root -> left);
+    int right = height(root -> right);
+
+    int res = max(left, right) + 1;
+
+    return res;
+}
+int main(){
+    node* root = NULL;
+     root = buildingtree(root);
+    
+    int heightoftree = height(root);
+    cout << "the height of the tree is " << heightoftree ;
+
+}
